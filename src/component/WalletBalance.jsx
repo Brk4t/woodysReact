cd5e665fc766f789b96ca693c3ca33e19b653e39
Brk4t <30989055+Brk4t@users.noreply.github.com>
2022-03-02 17:20:12 -0600

    designUpdate

--- src/component/WalletBalance.jsx
+++ src/component/WalletBalance.jsx
@@ -3,6 +3,7 @@ import { ethers } from "ethers";
 
 function WalletBalance() {
   const [balance, setBalance] = useState();
+  const [address = '', setAddress] = useState();
 
   const getBalance = async () => {
     const [account] = await window.ethereum.request({
@@ -11,12 +12,13 @@ function WalletBalance() {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const balance = await provider.getBalance(account);
     setBalance(ethers.utils.formatEther(balance));
+    setAddress(account)
   };
+  getBalance()
   return (
     <div className="card">
       <div>
-        <h5> Your Balance : {balance} </h5>
-        <button onClick={() => getBalance()}>Show My Balance </button>
+        <h5> Network : {window.ethereum.networkVersion} <br/> Address : {address.substring(0,5) + '...'+address.substring(address.length-3)} <br/> Balance : {Math.round(balance*1000)*0.001} MATIC </h5>
       </div>
     </div>
   );
--- src/component/home.jsx
+++ src/component/home.jsx
@@ -4,7 +4,7 @@ import { ethers } from "ethers";
 import WalletBalance from "./WalletBalance";
 import Woodys from "../abi/woodys.json";
 
-const contractAddress = "0x32d2b0EFc0E841e9CEAD6aaA732956bb07F9c5aE";
+const contractAddress = "0x417c59F050e5Bb2061D915331158892417b5113b";
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 
 const signer = provider.getSigner();
@@ -13,82 +13,92 @@ const woodyContract = new ethers.Contract(contractAddress, Woodys.abi, signer);
 
 const Home = () => {
   const [totalMinted, setTotalMinted] = useState(0);
+  const [arrayOfOwnedTokens = [], setArrayOfOwnedTokens] = useState(0);
+  const [account, setAccount] = useState(0)
 
   useEffect(() => {
     getCount();
+    getAccount();
   }, []);
+
+  const getAccount = async () => {
+    const [acc] = await window.ethereum.request({
+      method: "eth_requestAccounts"
+    });
+    setAccount(acc)
+    console.log("ACC +++++++++ " + acc)
+    if(acc != 0)
+    {
+    const arr = await woodyContract.getUserTokens(acc);
+    setArrayOfOwnedTokens(arr);
+    console.log("ARRAY  " + arr)
+    }
+  }
+
 
   const getCount = async () => {
     const count = await woodyContract.count();
-    console.log(parseInt(count));
     setTotalMinted(parseInt(count));
   };
 
-  return (
-    <div>
-      <h3> Metamask installed, now what</h3>
-      <WalletBalance />
-      <h1>NFT collection </h1>
-      <div className ="MyCollection">
-      {Array(totalMinted + 1)
-        .fill(0)
-        .map((_, i) => (
-          <div key={i}>
-            <NFTImage tokenId={i} />
-          </div>
-        ))}
-    </div>
-    </div>
-  );
-};
-
-function NFTImage(props) {
-  console.log("props " + props.tokenId)
-  const metadataURI = `../QxHASHAHASHASHASHAS/${props.tokenId}.json`;
-  const imageURI = '/img/'+props.tokenId+'.png';
-  
-  console.log(metadataURI)
-
-  const [isMinted, setIsMinted] = useState(false);
-  useEffect(() => {
-    getMintedStatus();
-  }, [isMinted]);
-
-  const getMintedStatus = async () => {
-    const result = await woodyContract.isContentOwned(metadataURI);
-    console.log(result);
-    setIsMinted(result);
-  };
-
   const mintToken = async () => {
+    
+    const metadataURI = `../QxHASHAHASHASHASHAS/${totalMinted}.json`;
     const connection = woodyContract.connect(signer);
     const addr = connection.address;
     const result = await woodyContract.payToMint( metadataURI, {
       value: ethers.utils.parseEther("0.05")
     });
     await result.wait();
+    window.location.reload();
     getMintedStatus();
   };
 
-  async function getURI() {
-    const uri = await woodyContract.tokenURI(props.tokenId);
-    return uri;
+  const displayCards = () =>
+  {
+    if(Array.isArray(arrayOfOwnedTokens))
+    {    
+      return arrayOfOwnedTokens
+      .map((_, i) => (
+        <div key={i} className="oneCard">
+          <NFTImage tokenId={i} />
+        </div>
+      ))
+    }
   }
+
+  return (
+    <div>
+      <div className="banner">
+      <div className="title">Woody's World</div>
+      <WalletBalance />
+      </div>
+      <div className="mint">
+      <button className='button-29' onClick={mintToken}>Mint 0.05 ETH</button>
+      </div>
+      <div className ="MyCollection">
+      <div className="title">My NFT collection </div>
+      <div className ="MyCards">
+      {displayCards()}
+        </div>
+    </div>
+    </div>
+  );
+};
+
+function NFTImage(props) {
+  console.log("props of NFTIMAGE " + props.tokenId)
+  const imageURI = '/img/'+props.tokenId+'.png';
+  
 
   return (
     <div >
       <img
-        src={isMinted ? imageURI : "https://jooinn.com/images/lock-12.png"}
-        width="200"
-        height="200"
+        src={imageURI}
+        width="400"
+        height="400"
       />
       <div >
-        <h5> ID #{props.tokenId}</h5>
-        {!isMinted ? (
-          <button onClick={mintToken}>Mint 0.05 ETH</button>
-        ) : (
-          <button onClick={getURI}>Taken! Show URI</button>
-        )}
       </div>
     </div>
   );
